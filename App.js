cfd8daf76e8cb01578c79feae949a045d7937a41
Nami23Shukla <shuklanami23@gmail.com>
2024-11-17 12:59:30 +0530

    React Comment and jsx

--- App.js
+++ App.js
@@ -1,24 +1,35 @@
-{
-    /*
-<div id="parent">
-    <div id= "child1">
-        <h1>hello world</h1>
-        <h1>hello world</h1>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const heading = (
+    <div> 
+    <h1>Hello World</h1>
     </div>
-    <div id = "child2">
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-    </div>
-</div>
- */
+
+);
+
+const Layout = () => {
+
+    const price1 = 2000;
+    const price2 = 3000;
+    const calculateprice = () =>{
+        return price1 + price2;
+    }
+
+    return(
+        <div>
+           
+            <h1>This is my product price:{calculateprice()}</h1>
+        </div>
+    )
 }
+// console.log(heading);
 
-const heading = React.createElement("h1", {id:"heading"},"Hello world");
-const divElement1= React.createElement("div",{id:"child1"},[heading,heading]);
-const divElement2= React.createElement("div",{id:"child2"},[heading,heading]);
+    
 
-const parentDivElement = React.createElement("div", {id:"parent"},[divElement1,divElement2]);
-
-console.log(heading);
+// const heading = React.createElement("h1", {id:"heading"},"Hello world");
+// console.log(heading);
+// const heading2 = <h1>Hello world JSX!!</h1>
+// console.log(heading2);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parentDivElement);
+// root.render(heading);
+root.render(<Layout/>);
